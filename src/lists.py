ea85b143c7312f633edc76be838bf8b743b8a772
Tuantrung <tuantrung03121997@gmail.com>
2023-03-06 22:59:44 +0700

    Done lists problem

---
+++ src/lists.py
@@ -0,0 +1,24 @@
+# import sys
+output = []
+
+if __name__ == '__main__':
+    N = int(input())
+    command_list = {
+    "insert": lambda x: output.insert(x[1], x[0]),
+    "print": lambda x: print(output),
+    "remove": lambda x: output.remove(x[0]),
+    "append": lambda x: output.append(x[0]),
+    "sort": lambda x: output.sort(),
+    "pop": lambda x: output.pop(),
+    "reverse": lambda x: output.reverse()
+    }
+    for _ in range(N):
+        string, *el = input().split()
+        command_list[string](list(map(int, el)))
+
+
+
+
+
+
+
