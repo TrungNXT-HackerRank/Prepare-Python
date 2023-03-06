b98ef60770356b2380393a781d24fd3417facb4a
Tuantrung <tuantrung03121997@gmail.com>
2023-03-06 23:58:30 +0700

    Done tuples problem

--- src/lists.py
+++ src/lists.py
@@ -4,7 +4,7 @@ output = []
 if __name__ == '__main__':
     N = int(input())
     command_list = {
-    "insert": lambda x: output.insert(x[1], x[0]),
+    "insert": lambda x: output.insert(x[0], x[1]),
     "print": lambda x: print(output),
     "remove": lambda x: output.remove(x[0]),
     "append": lambda x: output.append(x[0]),
